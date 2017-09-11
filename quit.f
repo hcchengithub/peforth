7d0b132f60559d6e7d479de6518c5073e719aae6
H.C. Chen <hcchen5600@gmail.com>
2017-09-11 23:46:20 +0800

    new function outport()

--- quit.f
+++ quit.f
@@ -9,3 +9,19 @@
 \ Run the command line commands
     
     <py> " ".join(sys.argv[1:]) </pyV> tib.insert
+
+\ My misc tools 
+
+    <py>
+    def outport(loc): 
+        # The input argument is supposed locals() of the caller.
+        # Make all local variables forth constants
+        # Examine locals after a <Py>...</Py> section 
+        # For studying maching learning, tersorflow, ... etc.
+        for i in loc: 
+            push(loc[i]) # vale
+            push(i) # variable name
+            execute('(constant)')
+    vm.outport = outport
+    </py>
+    
