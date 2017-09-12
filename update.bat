7209c453fa617873a15e6805d4a5d662a3efe790
H.C. Chen <hcchen5600@gmail.com>
2017-09-12 18:52:45 +0800

    at office

---
+++ update.bat
@@ -0,0 +1,7 @@
+set pythonlib=C:\Users\hcche\AppData\Local\Programs\Python\Python36\lib
+copy /y version.txt %pythonlib%\site-packages\peforth\
+copy /y projectk.py %pythonlib%\site-packages\peforth\
+copy /y __main__.py %pythonlib%\site-packages\peforth\
+copy /y __init__.py %pythonlib%\site-packages\peforth\
+copy /y peforth.f   %pythonlib%\site-packages\peforth\
+copy /y quit.f      %pythonlib%\site-packages\peforth\
