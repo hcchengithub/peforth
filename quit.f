e8d3c15b8b764b089359d47fde3cf1ccd4f68601
H.C. Chen <hcchen5600@gmail.com>
2018-01-21 19:31:06 +0800

    about to merge to master

--- quit.f
+++ quit.f
@@ -157,7 +157,7 @@
 
 \ Do selftest or run command-line
     
-    ' <selftest> :: enabled=True \ Master switch of selftest, True:on or False:off
+    ' <selftest> :: enabled=False \ Master switch of selftest, True:on or False:off
 
     py> vm.commandline trim ( commandLine ) 
     ?dup [if] 
