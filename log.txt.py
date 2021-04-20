51ca1637093b0cbc4f12cf65083cebaaf18f502c
HCChen <h.c._chen@wistron.com>
2021-04-20 10:00:57 +0800

    save

--- log.txt.py
+++ log.txt.py
@@ -3399,12 +3399,10 @@ peforth.main() # 從 python interpreter 切換進入 peforth
 [X] 15:21 2019/11/25 整理 peforth.f quit.f peforth.selftest 的關係，更有系統了。
     __init__.py 只 load 進基本的 peforth.f quit.f 其他的都由 quit.f 負責，使 quit.f 
     成為 eforth 系統的 main program, 統籌者。
-[X] 16:07 2019/11/25 一舉搞懂 pop(1) 
-    code test # ( a b c -- ) print given things
-        print(pop(), pop(), pop()) end-code
-    1 2 3 test
-    3 2 1 <-- 結果，顯示三個 pop() 是從左到右抓取 TOS 的。
-[X] Tests before a Release v1.24
+[X] 16:07 2019/11/25 一舉搞懂 pop(1) pop(0) in function argument 的順序
+    code test print(pop(),pop()) end-code // ( 1 0 -- ) 實驗搞清 pop() 順序 1 0 func :: (pop(1),pop(0)) 得 (1,0); 若 1 0 func :: (pop(),pop()) 則得 (0,1)
+    1 0 test .s \ --> 0 1
+    [X] Tests before a Release v1.24
     [X] setup.py 裡的 copy right 年份要改成 2019 
     *** 打包上 pypi.org 的方法 setup.bat 可以大簡化了。
         [V1.22之後的新版] 打包步驟  2018/12/16 11:02 
@@ -3670,6 +3668,7 @@ peforth.main() # 從 python interpreter 切換進入 peforth
         if id in peforth.bps: peforth.push(locals).ok(prompt, cmd="to _locals_")
     peforth.bp = bp
     peforth.bps = [i for i in range(1000)]
+    
 [X] 17:33 2020/12/07 配合 peforth.bp(22,locals()) 新增 bl be bd be* bd* 等指令
 [ ] 17:34 2020/12/07 release v1.26 to pypi 
     [X] 17:37 2020/12/07 先試試看 gom ok? --> Pass, 連 selftest 也都 pass. 
@@ -3810,9 +3809,88 @@ peforth.main() # 從 python interpreter 切換進入 peforth
     : paste py> IPython.lib.clipboard.win32_clipboard_get() tib.insert ; 
     // ( ... -- ... ) 執行 clipboard 裡的內容，jupyternotebook 進了 peforth prompt 特別需要此功能。
 
-
-
-
-
-
-
+[ ] 14:42 2021/04/12 peforth breakpoint 外掛如下，考慮加進 master 裡去 why not? 
+    import peforth
+    peforth.bps = [i for i in range(100)]  # 預設有這麼多 Breakpoint ID
+    def bp(id=None,locals=None):
+        # Breakpoint ID 不能超過 peforth.bps 保留，超過的無效。
+        if id==None: 
+            id = 0
+            prompt='bp> '
+        else:
+            prompt="{}>".format(id)
+        if id in peforth.bps: peforth.push(locals).ok(prompt, cmd="to _locals_")
+    peforth.bp = bp
+    peforth.push(bp).dictate("py: vm.bp=pop()")  # [ ] 忘了 vm.bp 有何用途？
+    peforth.dictate('''
+        \ ------ breakpoint ------------------------------------------------------------
+        : bp ." Usage: peforth.bp(11,locals())  # drop a breakpoint with ID=11" cr ;
+        : bl // ( -- ) List all breakpoints
+            __main__ :> peforth.bps 
+            <py>
+            bps = pop()
+            print('Disabled breakpoints:')
+            for i in range(len(bps)):
+                if not bps[i]: 
+                    print(i, end=' ')
+            print(); print('Enabled breakpoints:')
+            count = 0
+            for i in range(len(bps)):
+                if bps[i]: 
+                    print(i, end=' ')
+                    count += 1
+            print(); print('Enabled breakpoints count: {}/{}'.format(count,len(bps)))
+            </py> cr ;
+            /// Breakpoint commands:
+            ///	  bl  - list all breakpoints (capital BL is white space) 
+            ///	  be  - enable breakpoints, e.g. be 1 2 3 
+            ///	  bd  - disable breakpoints, e.g. bd 1 2 3 
+            ///	  be* - enable all breakpoints
+            ///	  bd* - disable all breakpoints 
+            /// Also: 
+            ///   for i in [11,22,33]: peforth.bps[i]=0	 # disable breakpoints 11,22,33 
+            ///   for i in [11,22,33]: peforth.bps[i]=i	 # enable  breakpoints 11,22,33 
+        
+        : bd // ( <1 2 3 4...> -- ) Disable listed breakpoints 
+            CR word ( line ) __main__ :> peforth.bps ( line bps )
+            <py>
+            line, bps = pop(1), pop(0)
+            points = map(int, line.split(' '))
+            for i in points: bps[i] = 0
+            </py> ; 
+            ' bl :> comment last :: comment=pop(1)
+        
+        : be // ( <1 2 3 4...> -- ) Enable listed breakpoints 
+            CR word ( line ) __main__ :> peforth.bps ( line bps ) 
+            <py>
+            line, bps = pop(1), pop(0)
+            points = map(int, line.split(' '))
+            for i in points: bps[i] = i
+            </py> ; 
+            ' bl :> comment last :: comment=pop(1)
+        
+        : bd* // ( -- ) Disable all breakpoints 
+            __main__ :> peforth.bps	 ( bps ) 
+            <py>
+            bps = pop()
+            for i in range(len(bps)): bps[i] = 0
+            </py> ;
+            ' bl :> comment last :: comment=pop(1)
+        
+        : be* // ( -- ) Enable all breakpoints 
+            __main__ :> peforth.bps	 ( bps ) 
+            <py>
+            bps = pop()
+            for i in range(len(bps)): bps[i] = i
+            </py> ;
+            ' bl :> comment last :: comment=pop(1)
+        \ ------ xtack ------------------------------------------------------------
+        [] value xstack xstack py: vm.xstack=pop() // ( -- array ) The xstack. 掛進 vm 就可以直接以 py> xstack 取用。
+        : x@ xstack :> [-1] ; // ( -- n ) Get TOS of the xstack
+        : x> xstack :> pop() ; // ( -- n ) Pop the xstack
+        : >x xstack :: append(pop()) ; // ( n -- ) Push n into the xstack
+        : .sx xstack . ; // ( -- ) List xstack 
+        : xdrop x> drop ; // ( X: ... a -- X: ... ) drop xstack 
+        : xdropall [] to xstack ; // ( X: ... -- X: empty ) clear xstack 
+        ''')
+    %f version ==>
