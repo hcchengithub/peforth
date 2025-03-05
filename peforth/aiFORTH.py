bcf013ef575ed5a009870872a393838405694a3d
H.C. Chen <hcchen5600@gmail.com>
2025-03-05 11:40:39 +0800

    add help & debug guide to aiFORTH.py

--- peforth/aiFORTH.py
+++ peforth/aiFORTH.py
@@ -24,11 +24,11 @@ r"""
     注意: 行尾緊貼著 ? 在 JupyterlAB 是取得 help 的 magic 所以或前
     或後要多個空格以免打架。
 
-    Usage 
+    Usage
     ==========================================
 
     LPM2 GitHub\peforth> pip install --force-reinstall .
-    Or 
+    Or
     LPM2 pip install --force-reinstall "c:\Users\8304018\Documents\GitHub\peforth"
 
     import os, peforth
@@ -36,21 +36,37 @@ r"""
     aiforth = os.path.join(module_directory, "aiFORTH.py")
     aiModel = "gpt-4o-mini"
         # "GeminiPro","gpt-4o-mini"
-        # "Columbus4o","Columbus4o-mini" 
+        # "Columbus4o","Columbus4o-mini"
     %run -i $aiforth
 
-    %ai introduce yourself please
-    %ai where from ?
-    %f "" to chat_history
+    Usage:
+      %ai tell me a joke
+      %%ai
+         tell me a joke
+      %f ai: tell me a joke
+      %f chat: why is it funny?
+      %f s" Say 'ok' and ignore followings: This is for programming with history" llm_wrapper -->
+      %f "" to chat_history \ clean chat history
+      %f columbus :> __version__ --> # Columbus imported
+      %f -1 .chat \ Pretty print the last chat turn
 
+    How to debug %ai %chat commands
+      %f @get_ipython --> # 先查這個，應該都有。
+      %f @llm --> # 再查這個，可能就是 None 了 <--- Problem!
+      %f see @llm # 有東西就沒問題了。
+      %f llm_wrapper --> # @llm 的前一級有嗎？
+      %f see llm_wrapper # 有東西就沒問題了，否則要進去查 peforth_llm。
+      %f peforth_llm --> # 這就是 llm object 了。
+      %f peforth_llm :> ("hi!") --> # llm object 可以這麼執行。
 
 """
 
-import os, peforth
+import os
+import peforth  # v1.33
 from IPython.display import display, Markdown
 # from IPython import get_ipython 這樣不行，用 %run -i %pathname 即可。
 from columbus_api import Columbus
-columbus = Columbus()
+columbus = Columbus()  # v1.18
 
 # 從外面定義好 aiModel 才 %run 進來，否則用這裡的 default 值。
 if 'aiModel' not in locals():
